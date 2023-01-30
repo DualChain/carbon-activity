006cef9b2738f538182e0bd07cf23c7bf147a094
exp-er <exp.er@outlook.com>
2023-01-30 16:52:36 +0000

    main-page

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from data.daily import *
 from data.weekly import *
 from data.monthly import *
 from data.annually import *
+from data.output import *
+from PIL import Image, ImageDraw, ImageFont
 
 st.markdown("## Carbon Activity - A Dualchain Initiative")
 
@@ -19,3 +21,21 @@ intro.markdown(
     - Share it with friends and family!
     '''
 )
+
+
+text1 = "Create Image"
+text2 = 'With Python'
+img_name = 'Output.png'
+background= Image.open('./data/Background.jpg')
+color = 'dark_blue'  # grey,light_blue,blue,orange,purple,yellow,green
+font = 'Roboto-Bold.ttf'
+background = write_image(background, colors[color], text1, text2)
+background.save(img_name)
+
+with open("Output.png", "rb") as file:
+    btn = st.download_button(
+            label="Download Output",
+            data=file,
+            file_name="Output.png",
+            mime="image/png"
+          )
